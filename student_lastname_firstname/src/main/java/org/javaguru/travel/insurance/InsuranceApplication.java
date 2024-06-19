f3be39f9e0c387f96721a66753163d7d47825cf5
llsquishy <tiredlosing@gmail.com>
2024-06-20 01:36:25 +0300

    STEP 20 - PULL

--- student_lastname_firstname/src/main/java/org/javaguru/travel/insurance/InsuranceApplication.java
+++ student_lastname_firstname/src/main/java/org/javaguru/travel/insurance/InsuranceApplication.java
@@ -2,7 +2,6 @@ package org.javaguru.travel.insurance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class InsuranceApplication {
 
